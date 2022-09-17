7ba751e2c50fad9b39d4a4a38213a65d9b622538
hugoscoder <hugoscoder@gmail.com>
2022-09-17 13:44:26 +0200

    initial commit

--- src/main/kotlin/de/toby/uhc/command/BackpackCommand.kt
+++ src/main/kotlin/de/toby/uhc/command/BackpackCommand.kt
@@ -7,18 +7,19 @@ import net.axay.kspigot.commands.command
 import net.axay.kspigot.commands.runs
 import net.axay.kspigot.gui.openGUI
 import net.axay.kspigot.sound.sound
+import org.bukkit.ChatColor
 import org.bukkit.Sound
-import org.bukkit.entity.Player
 
 object BackpackCommand {
 
     fun enable() {
         command("backpack") {
             runs {
-                if(Game.current !is Ingame) return@runs
-
-                player.openGUI(player.getTeam()?.backpack ?: return@runs)
-                player.sound(Sound.BLOCK_CHEST_OPEN)
+                if (Game.current !is Ingame) player.sendMessage("${ChatColor.RED}The game has not started yet")
+                else {
+                    player.openGUI(player.getTeam()?.backpack ?: return@runs)
+                    player.sound(Sound.BLOCK_CHEST_OPEN)
+                }
             }
         }
     }
